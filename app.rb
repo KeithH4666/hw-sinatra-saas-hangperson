377d3c1051b5ad531fbd8a9b168c47ee54ba3122
EC2 Default User <ec2-user@ip-172-31-11-203.us-east-2.compute.internal>
2019-02-06 16:38:03 +0000

    added /create to f

--- app.rb
+++ app.rb
@@ -50,7 +50,6 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
     erb :show # You may change/remove this line
   end
   
