b3a4d83cc573280dcd369a99322d1cd1057614be
EC2 Default User <ec2-user@ip-172-31-11-203.us-east-2.compute.internal>
2019-02-10 14:57:22 +0000

    added redirects for win/loose

--- app.rb
+++ app.rb
@@ -58,6 +58,15 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
+    
+    if(@game.instance_variable_get(:@check_win_or_lose) == :win)
+      redirect '/win'
+    end
+
+    if(@game.instance_variable_get(:@check_win_or_lose) == :lose)
+      redirect '/lose'
+    end
+    
     erb :show # You may change/remove this line
   end
   
